80b1280d22082b17572e6f69685d849687064eae
helloxms <xums.cn@gmail.com>
2019-02-12 07:31:16 +0000

    set update frame to low low low

--- CarND-Capstone/ros/src/waypoint_updater/waypoint_updater.py
+++ CarND-Capstone/ros/src/waypoint_updater/waypoint_updater.py
@@ -23,7 +23,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 100 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 50 # Number of waypoints we will publish. You can change this number
 MAX_DECEL = 0.5
 
   
@@ -58,7 +58,7 @@ class WaypointUpdater(object):
 
 
     def loop(self):
-        rate = rospy.Rate(50)
+        rate = rospy.Rate(2)
         while not rospy.is_shutdown():
 		
 		if self.pose and self.base_waypoints:
